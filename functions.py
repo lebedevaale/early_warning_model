8f0afdc7ae46758b37786bab5a5a511ab8185131
lebedevaale <58344703+lebedevaale@users.noreply.github.com>
2024-06-24 22:39:44 +0300

    Added simulations for random and sand pile series

--- functions.py
+++ functions.py
@@ -1,13 +1,17 @@
+import copy
 import operator
 import numpy as np
 import scipy as sp
 import pandas as pd
 from tqdm import tqdm
+import networkx as nx
 import statsmodels.api as sm
 import sklearn.metrics as metrics
 import plotly.graph_objects as go
 import sklearn.model_selection as modsel
 from plotly.subplots import make_subplots
+
+np.random.seed(0)
 
 #---------------------------------------------------------------------------------------
 
@@ -381,3 +385,329 @@ def model(data,
                             pass
             
     return res
+
+#---------------------------------------------------------------------------------------
+    
+def generate_random_series(length:int, 
+                           number:int, 
+                           mean:float = None, 
+                           sigma:float = None, 
+                           type:str = 'normal') -> pd.DataFrame:
+    
+    """
+    Function for the generation of random series
+    
+    Inputs:
+    ---------
+    length : int
+        Length of the series
+    number : int
+        Number of the series
+    mean : float
+        Mean of the distribution
+    sigma : float
+        Sigma of the distribution
+    type : str = 'normal'
+        Type of the series, may be 'normal', 'lognormal' or 'rw'
+    
+    Returns:
+    ---------
+    res : pd.DataFrame
+
+    Raises:
+    ---------
+    ValueError: If type of the series is not 'normal', 'lognormal' or 'rw'
+    """
+
+    # Normal distribution
+    if type == 'normal':
+        res = pd.DataFrame(np.random.normal(loc = mean, scale = sigma, size = (length, number)), 
+                           columns = [str(i) for i in range(number)])
+    
+    # Lognormal distribution
+    elif type == 'lognormal':
+        res = pd.DataFrame(np.random.lognormal(mean = mean, sigma = sigma, size = (length, number)), 
+                           columns = [str(i) for i in range(number)])
+    # Random walk
+    elif type == 'rw':
+        res = pd.DataFrame(np.cumsum(np.random.randn(length, number), axis = 1), columns = [str(i) for i in range(number)])
+
+    else:
+        raise ValueError('Incorrect type of the series')
+    
+    return res
+
+#---------------------------------------------------------------------------------------
+
+def graph_generation(graph_type:str,
+                     number_of_nodes:int, 
+                     BA_connect:int = None, 
+                     ER_prob:float = None,
+                     CL_average:int = None,
+                     silent:bool = True) -> nx.Graph:
+    
+    """
+    Function that generates a graph based on the specified graph type.
+
+    Inputs:
+    ---------
+    graph_type : str
+        The type of graph to generate. Valid values are 'BA', 'ER', and 'CL'.
+    number_of_nodes : int
+        The number of nodes in the graph.
+    BA_connect : int = None
+        The number of connections in the Barabasi-Albert graph. Required if graph_type is 'BA'.
+    ER_prob : float = None
+        The probability of an edge in the Erdos-Renyi graph. Required if graph_type is 'ER'.
+    CL_average : int = None
+        The average degree in the Chung-Lu graph. Required if graph_type is 'CL'.
+    silent : bool = True
+        If True, suppresses the print of graph size (number of edges).
+
+    Returns:
+    ---------
+    G : nx.Graph
+        The generated graph.
+
+    Raises:
+    ---------
+    ValueError: If the graph_type is not one of 'BA', 'ER', or 'CL'.
+    """
+    
+    if graph_type == 'BA':
+        G = nx.barabasi_albert_graph(number_of_nodes, BA_connect)
+    elif graph_type == 'ER':
+        G = nx.erdos_renyi_graph(number_of_nodes, ER_prob)
+    elif graph_type == 'CL':
+    # Model's Parameters: Generate a random Chung-Lu graph with average degree d, max degree m, and power-law degree distribution with exponent gamma
+    # Source: https://github.com/ftudisco/scalefreechunglu/blob/master/python/example.py
+        gamma = 2.2
+        m = number_of_nodes ** 0.4
+        p = 1 / (gamma - 1)
+        c = (1 - p) * CL_average * (number_of_nodes ** p)
+        i0 = (c / m) ** (1 / p) - 1
+        w = [c / ((i + i0) ** p) for i in range(number_of_nodes)]
+        G = nx.expected_degree_graph(w)
+    else:
+        raise ValueError("Wrong graph_type. Choose BA, ER or CL.")
+    
+    if silent == False: 
+        print(G.size())
+
+    return G
+
+#---------------------------------------------------------------------------------------
+
+def grain_generator(number_of_nodes:int, 
+                    number_of_days:int, 
+                    dist:str = 'uni') -> list:
+    
+    """
+    Generate grains to be put into the sand pile based on the specified distribution for a given number of nodes and days.
+
+    Inputs:
+    ---------
+    number_of_nodes : int
+        The number of nodes to generate grains for.
+    number_of_days : int
+        The number of days for which grains are generated.
+    dist : str = 'uni'
+        The type of distribution to use for generating grains. Default is 'uni'.
+        Other options: 'expon' for exponential, 'par' for Pareto.
+
+    Returns:
+    ---------
+    new_grains : list
+        A list of generated grains based on the specified distribution.
+
+    Raises:
+    ---------
+    ValueError: If the distribution type is not one of 'uni', 'expon' or 'par'.
+    """
+    
+    # Create list to fday
+    new_grains = []
+    
+    # Generate grains depending on the distribution
+    if dist == 'uni':
+        for d in range(number_of_days):
+            grain = np.random.randint(number_of_nodes)
+            new_grains.append([grain])
+    elif dist in ['expon', 'par']:
+        if dist == 'expon':
+            num_of_grains = np.around(np.random.exponential(size = number_of_days))
+        else:
+            num_of_grains = np.around(np.random.pareto(a = 2, size = number_of_days))
+        for d in range(number_of_days):
+            grains = []
+            for g in range(int(num_of_grains[d]) + 1):
+                grain = np.random.randint(number_of_nodes)
+                grains.append(grain)
+            new_grains.append(grains)
+    else:
+        raise ValueError('Wrong distribution type. Choose uni, expon or par.')
+    
+    return new_grains
+
+#---------------------------------------------------------------------------------------
+
+def spread_model(G:nx.Graph, 
+                 ones:list, 
+                 falls_d:int, 
+                 d:int, 
+                 node:list, 
+                 crit:int, 
+                 type:str = 'BTW'):
+    
+    """
+    Function for the implementation of different spread models on the graph.
+    
+    Inputs:
+    ---------
+    G : nx.Graph 
+        The graph on which the fall model is applied.
+    ones : list 
+        List of nodes with only one edge.
+    falls_d : int 
+        The number of iterations.
+    d : int
+        The current day.
+    node : list
+        The node to be processed.
+    crit : int
+        The critical value.
+    type : str
+        The type of model to be used. Default is 'BTW'. Other option: 'MA'.
+    
+    Returns:
+    ---------
+        tuple: A tuple containing the updated graph G and the updated number of iterations falls_d.
+    
+    Raises:
+    ---------
+    ValueError: If the type of model is not one of 'BTW' or 'MA'.
+    """
+
+    # Increase the number of iteration
+    falls_d += 1
+
+    # Iterate over nodes that have more than one edge
+    if node[0] not in ones:
+        neighbors = [n for n in G.neighbors(node[0])]
+        remains = copy.copy(crit)
+        for neighbor in G.nodes(data=True):
+            if neighbor[0] in neighbors:
+                # Spread the grains over the neighbors
+                if type == 'BTW':
+                    # Deterministic model
+                    neighbor[1]['day'+str(d+1)] += 1
+                elif type == 'MA':
+                    # Stochastic model
+                    n = np.random.randint(0, remains)
+                    remains -= n
+                    neighbor[1]['day'+str(d+1)] += n
+                else:
+                    raise ValueError('Wrong model type. Choose BTW or MA.')
+    
+    # Update the number of grains in the node
+    node[1]['day'+str(d+1)] -= crit
+
+    return G, falls_d
+
+#---------------------------------------------------------------------------------------
+
+def spread(model:str,
+           G:nx.Graph,
+           number_of_days:int,
+           new_grains:list,
+           ad_dissipation:bool = False,
+           neutral_state:bool = False,
+           new_grains_plus:list = None,
+           new_grains_minus: list = None,
+           silent:bool = True) -> list:
+    
+    """
+    Function for modelling of sand grain spread with Bak-Tang-Wiesenfeld and Manna on random graphs.
+
+    Inputs:
+    ---------
+    model : str 
+        The type of spread model to be used.
+    G : nx.Graph 
+        The graph on which the spread model is applied.
+    number_of_days : int
+        The number of days to simulate the spread.
+    new_grains : list
+        A list of lists, where each inner list contains the nodes where new grains are added on each day.
+    ad_dissipation : bool
+        If True, the spread model includes ad dissipation (+2-1 instead of +1-0).
+    neutral_state : bool
+        If True, the spread model includes neutral state (+1-1 instead of +1-0).
+    new_grains_plus : list
+        A list of lists, where each inner list contains the nodes where new grains are added on each day.
+    new_grains_minus : list
+        A list of lists, where each inner list contains the nodes where new grains are subtracted on each day.
+
+    Returns:
+    falls : list 
+        A list containing the number of falls for each day.
+    """
+
+    # Initialize variables
+    falls = []
+    ones = []
+    deg = []
+
+    # Get the degree of each node
+    degrees = [[node, val]  for (node, val) in G.degree()]
+    for degree in degrees:
+        deg.append(degree[1])
+        if degree[1] == 1:
+            ones.append(degree[0])
+
+    # Create status dataframe
+    status = pd.DataFrame()
+    for j in range(0, number_of_days):
+        status['day'+str(j)] = np.zeros(G.number_of_nodes())
+
+    # Set node attributes
+    node_attr = status.to_dict('index')
+    nx.set_node_attributes(G, node_attr)
+
+    # Simulate the spread
+    for d in tqdm(range(0, number_of_days-1), disable = silent):
+        falls_d = 0
+
+        # Add/subtract grains
+        if ad_dissipation == True:
+            for node in G.nodes(data=True):
+                if node[0] in new_grains[d]:
+                    node[1]['day'+str(d)] += 1.0
+                if node[0] in new_grains_plus[d]:
+                    node[1]['day'+str(d)] += 1.0
+                if node[0] in new_grains_minus[d]:
+                    node[1]['day'+str(d)] -= 1.0          
+                node[1]['day'+str(d+1)] += node[1]['day'+str(d)]
+        elif neutral_state == True:
+            for node in G.nodes(data=True):
+                if node[0] in new_grains[d]:
+                    node[1]['day'+str(d)] += 1.0
+                if node[0] in new_grains_minus[d]:
+                    node[1]['day'+str(d)] -= 1.0          
+                node[1]['day'+str(d+1)] += node[1]['day'+str(d)]    
+        else:
+            for node in G.nodes(data=True):
+                if node[0] in new_grains[d]:
+                    node[1]['day'+str(d)] += 1.0
+                node[1]['day'+str(d+1)] += node[1]['day'+str(d)]
+
+        # Spread grains
+        for node in G.nodes(data=True):
+            if d <= (number_of_days-1) and node[1]['day'+str(d)] >= deg[node[0]] and deg[node[0]] > 0:
+                G, falls_d = spread_model(G, ones, falls_d, d, node, deg[node[0]], type = model)
+
+        # Append number of falls
+        if d <= number_of_days-2:
+            falls.append(falls_d)
+
+    return falls
